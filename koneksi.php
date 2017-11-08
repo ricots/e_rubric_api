a63d27d67dd5079d810642de5d36083f24b39969
ricots <ricots47@gmail.com>
2017-11-08 09:43:47 +0700

    e_rubric api

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST','localhost');
-	define('USER','mydb1675');
-	define('PASS','mydeveloper18');
+	define('USER','');
+	define('PASS','');
 	define('DB','mydb1675_e_rubic');
 	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
 ?>	
